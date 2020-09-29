d365946b00d73ac6c141f50e7fa4b53e679b56ee
gley <gley.mobi@gmail.com>
2020-09-29 10:45:09 +0300

    project added

---
+++ Assets/GleyPlugins/Share/Manager.cs
@@ -0,0 +1,23 @@
+﻿namespace GleyShare
+{
+    public class Manager
+    {
+        public static void ShareRoom(string text)
+        {
+#if UseNativeShare
+        NativeShare share = new NativeShare();
+        share.SetText(text);
+        share.Share();
+#endif
+        }
+
+        public static void SharePicture(string path, string filename)
+        {
+#if UseNativeShare
+        NativeShare share = new NativeShare();
+        share.AddFile(path + "/" + filename);
+        share.Share();
+#endif
+        }
+    }
+}

40f13c2637c70cffd4033e6f3b53acab2ba7e48e
Gley <gley.mobi@gmail.com>
2020-10-14 15:47:57 +0300

    Tutorial done

--- Assets/Scenes/TestScript.cs
+++ Assets/Scenes/TestScript.cs
@@ -5,36 +5,43 @@ public class TestScript : MonoBehaviour
 {
     public Image pictureHolder;
 
+
+    //Hide picture holder
     void Start()
     {
+        //this method will show and hide the picture holder
         ShowPictureHolder(false);
     }
 
-    void ShowPictureHolder(bool visible)
+    private void ShowPictureHolder(bool visible)
     {
         pictureHolder.gameObject.SetActive(visible);
     }
 
+    //Create take screenshot method. It will be called by the button
     public void TakeScreenshot()
     {
-        Debug.Log("Screenshot");
         GleyShare.Manager.CaptureScreenshot(ScreenshotCaptured);
     }
 
+    //called after a screenshot is captured
     private void ScreenshotCaptured(Sprite sprite)
     {
         if(sprite!=null)
         {
+            //set captured image on picture holder and enable it
             pictureHolder.sprite = sprite;
             ShowPictureHolder(true);
         }
     }
 
+    //create the cancel method
     public void Cancel()
     {
         ShowPictureHolder(false);
     }
 
+    //Create share method
     public void Share()
     {
         GleyShare.Manager.SharePicture();
